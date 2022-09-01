1a923e5da989fe913a280d0e879377d606ec9c9b
antonije-pavlovic <antonije.work@gmail.com>
2022-09-01 19:10:18 +0200

    useer global error middleware

--- src/services/api/api.service.ts
+++ src/services/api/api.service.ts
@@ -1,9 +1,11 @@
 import express, { Express } from 'express';
+import formatError from './middlewares/error/format-errorr';
 import routes from './routes';
 
 // express setup
 const app: Express = express();
 
 app.use(routes);
+app.use(formatError);
 
 export default app;
