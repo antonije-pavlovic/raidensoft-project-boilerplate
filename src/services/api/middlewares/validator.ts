aca54c950ebb6259bc51c6fde7a36a927c0feb58
antonije-pavlovic <antonije.work@gmail.com>
2022-09-02 18:10:50 +0200

    refactor validator function

--- src/services/api/middlewares/validator.ts
+++ src/services/api/middlewares/validator.ts
@@ -1,20 +1,29 @@
 import { NextFunction, Request, Response } from 'express';
-import { validationResult } from 'express-validator';
+import { validationResult, ValidationChain, ValidationError } from 'express-validator';
 import UnprocessableError from '../../../errors/custom/unprocessable.error';
 
-export default function validator(validatorsChain) {
+export default function validator(validatorsChain: ValidationChain[]) {
 
   return async(req: Request, _res: Response, next: NextFunction) => {
-    await Promise.all(validatorsChain.map(validation => validation.run(req)));
-    const errors = validationResult(req);
+    const promiseArr: Array<Promise<any>> = [];
 
+    for(let i = 0; i < validatorsChain.length; i++) {
+      promiseArr.push(validatorsChain[i].run(req));
+    }
+
+    await Promise.all(promiseArr);
+
+    const errors = validationResult(req);
     if(!errors.isEmpty()) {
-      const formatedErrors = errors.array().map(error => {
-        return {
-          path: `${error.location}.${error.param}`,
-          code: `${error.msg.split(' ').join('_')}`
-        };
-      });
+      const formatedErrors: Array<{path: string, code: string}> = [];
+      const validatedErrors: ValidationError[] = errors.array();
+
+      for(let i = 0; i < validatedErrors.length; i++) {
+        formatedErrors.push({
+          path: `${validatedErrors[i].location}.${validatedErrors[i].param}`,
+          code: `${validatedErrors[i].msg.split(' ').join('_')}`
+        });
+      }
 
       return next(new UnprocessableError(formatedErrors));
     }
--- src/services/api/routes/user.ts
+++ src/services/api/routes/user.ts
@@ -3,6 +3,8 @@ import UserController from '../../user/user.controller';
 import catcheError from '../../../errors/catch-error';
 import { validateUser } from '../../user/user.validate';
 
+// TODO: Should we create class for this file UserRoute
+
 const router = Router();
 
 router.get('/',
@@ -14,6 +16,8 @@ router.post('/',
   catcheError(UserController.create)
 );
 
-router.put('/:id', catcheError(UserController.update));
+router.put('/:id',
+  catcheError(UserController.update)
+);
 
 export default router;
